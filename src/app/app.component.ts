3f0e36091e61b4b5f527eff917106a5d5ce2f3ad
dandax <daniel.dyck@gmail.com>
2023-11-13 11:09:29 -0500

    added tw function for host binding class sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+
+const tw = (strings: TemplateStringsArray, ...values: any[]) => String.raw({ raw: strings }, ...values);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  host: {
-    class: 'relative min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12 flex',
-  },
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostBinding('class')
+  get class() {
+    return tw`relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12`;
+  }
+}
